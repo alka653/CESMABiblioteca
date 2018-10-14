0343ba67df5430005ad0404dfc53335722fea9e1
alka653 <alka65@hotmail.com>
2018-10-14 18:52:45 -0500

    change presentation

--- cesma_library_database/apps/prestamo/urls.py
+++ cesma_library_database/apps/prestamo/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url, patterns
 from .views import *
 
 urlpatterns = patterns('cesma_library_database.apps.prestamo.views',
-	url(r'^$', login_required(PrestamoListLibro.as_view()), name = 'prestamo_libro'),
+     #url(r'^$', login_required(PrestamoListLibro.as_view()), name = 'prestamo_libro'),
+	url(r'^$', PrestamoListLibro.as_view(), name = 'prestamo_libro'),
 	url(r'^prestar/(?P<libro_isbn>[-\w]+)/$', login_required(PrestarLibro.as_view()), name = 'prestar_libro'),
 	url(r'^prestar/(?P<pk>\d+)/devolver/$', login_required(PrestarLibroDevolver), name = 'prestamo_devolver'),
 )
